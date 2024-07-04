dace5556044ddf268c76e6afb0e9a4b65f7127e5
Ren-451 <renren.g69@gmail.com>
2024-07-04 11:15:32 +0800

    form submission event listener

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const todoForm = document.getElementById('todo-form');
 const todoInput = document.getElementById('todo-input');
 const todoList = document.getElementById('todo-list');
+
+todoForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const newTask = todoInput.value;
+
+    if (newTask === '') {
+        alert('Enter a task.');
+        return;
+    }
+
+    todoInput.value = '';
+})
