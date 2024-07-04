028ef0a6f7eea63f9416ede7b0264e789c2fbb74
Ren-451 <renren.g69@gmail.com>
2024-07-04 14:03:53 +0800

    bug fix

--- index.js
+++ index.js
@@ -7,95 +7,94 @@ const todoList = document.getElementById('todo-list');
 todoForm.addEventListener('submit', function(event) {
     event.preventDefault();
     const newTask = todoInput.value;
-
+  
     if (newTask === '') {
-        alert('Enter a task.');
+        alert('Please enter a task!');
         return;
     }
-
     todoInput.value = '';
     addTask(newTask);
-});
+  });
 
 // add task
 
 function addTask(task) {
-    const editButton = document.createElement('button');
-    editButton.textContent = 'Edit';
-    listItem.appendChild(editButton);
-
     const listItem = document.createElement('li');
-    listItem.textContent = task;
     const taskText = document.createElement('span');
     taskText.textContent = task;
     listItem.appendChild(taskText);
-
+  
     const checkBox = document.createElement('input');
     checkBox.setAttribute('type', 'checkbox');
     listItem.appendChild(checkBox);
-
+  
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
     listItem.appendChild(deleteButton);
-
+  
     todoList.appendChild(listItem);
+  
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    listItem.appendChild(editButton);
+  
+    checkBox.addEventListener('change', function() {
+        if (this.checked) {
+            taskText.style.textDecoration = 'line-through';
+        } else {
+            taskText.style.textDecoration = 'none';
+        }
+    });
+
+    // delete
+
+    deleteButton.addEventListener('click', function() {
+        todoList.removeChild(listItem);
+    });
+    
+    // edit
+
+    editButton.addEventListener('click', function() {
+        const isEditing = listItem.classList.contains('editing');
+   
+        if (isEditing) {
+           
+            taskText.textContent = this.previousSibling.value;
+            listItem.classList.remove('editing');
+            editButton.textContent = 'Edit';
+        } else {
+           
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.value = taskText.textContent;
+            listItem.insertBefore(input, taskText);
+            listItem.removeChild(taskText);
+            listItem.classList.add('editing');
+            editButton.textContent = 'Save';
+        }
+    });
+
+    saveTasksToLocalStorage();
+
 }
-
-// task completion 
-
-checkBox.addEventListener('change', function() {
-    if (this.checked) {
-        taskText.style.textDecoration = 'line-through';
-    }
-    else {
-        taskText.style.textDecoration = 'none';
-    }
-});
-
-// delete
-deleteButton.addEventListener('click', function() {
-    todoList.replaceChild(listItem);
-});
-
-// edit
-
-editButton.addEventListener('click', function() {
-    const isEditing = listItem.classList.contains('editing');
-
-    if (isEditing) {
-        taskText.textContent = this.previousSibling.value;
-        listItem.classList.remove('editing');
-        editButton.textContent = 'Edit';
-    }
-
-    else {
-        const input = document.createElement('input');
-        input.type = 'text';
-        input.value = taskText.textContent;
-        listItem.insertBefore(input, taskText);
-        listItem.removeChild(taskText);
-        listItem.classList.add('editing');
-        editButton.textContent = 'Save';
-    }
-});
 
 // save tasks to local
 
-function saveTasksToLocalStorage(){
+function saveTasksToLocalStorage() {
     const tasks = [];
     document.querySelectorAll('#todo-list li').forEach(task => {
         const taskText = task.querySelector('span').textContent;
         const isCompleted = task.classList.contains('completed');
-        tasks.push({ text: taskText, completed: isCompleted});
+        tasks.push({ text: taskText, completed: isCompleted });
     });
-    localStorage.setItem('tasks', JSON,stringify(tasks));
-}
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
 
 // load task from local
 
 document.addEventListener('DOMContentLoaded', function() {
-    const saveTasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    saveTasks.forEach(task => {
+    const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    savedTasks.forEach(task => {
         addTask(task.text);
     });
 });
